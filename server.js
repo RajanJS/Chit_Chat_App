02fb978b729a6ee04696296d409263416a580063
RajanMaharjan <razanmaharjan@gmail.com>
2016-02-26 23:42:21 +0545

    Add front-end code to submit msg

--- server.js
+++ server.js
@@ -6,22 +6,22 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname + '/public'));
 
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
 	console.log('User connected via socket.io!');
 
-	socket.on('message',function(message){
-		console.log('Message received'+message.text);
+	socket.on('message', function(message) {
+		console.log('Message received >> ' + message.text);
 
-		socket.broadcast.emit('message',message);
+		socket.broadcast.emit('message', message);
 	});
 
-	socket.emit('message',{
+	socket.emit('message', {
 		text: 'Welcome to the Chit-Chat!'
 	});
 });
 
-http.listen(PORT,function () {
+http.listen(PORT, function() {
 	console.log('Server started!');
 });
