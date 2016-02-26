a32fea190aff0acc7fe6a9739b1361177db246f1
RajanMaharjan <razanmaharjan@gmail.com>
2016-02-27 00:48:05 +0545

    Show timestamp on UI

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var moment = require('moment');
 
 app.use(express.static(__dirname + '/public'));
 
@@ -14,11 +15,13 @@ io.on('connection', function(socket) {
 	socket.on('message', function(message) {
 		console.log('Message received >> ' + message.text);
 
+		message.timestamp = moment().valueOf();
 		io.emit('message', message);
 	});
 
 	socket.emit('message', {
-		text: 'Welcome to the Chit-Chat!'
+		text: '  Welcome to the Chit-Chat!',
+		timestamp: moment().valueOf()
 	});
 });
 
