f6e0bae6142d53aefcb316d688cf351572680f15
RajanMaharjan <razanmaharjan@gmail.com>
2016-02-27 01:33:00 +0545

    Add name support with message

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,10 @@
 'use strict';
 
+var name = getQueryVariable('name') || 'Anonymous';
+var room = getQueryVariable('room');
 var socket = io();
+
+console.log(name + ' want\'s to join ' +room);
 
 socket.on('connect', function() {
 	console.log('Connected to socket.io server!');
@@ -9,11 +13,12 @@ socket.on('connect', function() {
 socket.on('message', function(message) {
 	
 	var momentTimestamp = moment.utc(message.timestamp);
-
+	var $message = jQuery('.messages');
 	console.log('New message:');
 	console.log(message.text);
 
-	jQuery('.messages').append('<p><strong>'+ momentTimestamp.local().format('h:mm a')+': </strong>'+ message.text +'</p>');
+	$message.append('<p><strong>' + message.name +' '+ momentTimestamp.local().format('h:mm a') +'</strong></p>');
+	$message.append('<p>'+ message.text +'</p>');
 });
 
 //handles submitting of new message
@@ -25,6 +30,7 @@ $form.on('submit', function(event) {
 	var $message = $form.find('input[name=message]');
 
 	socket.emit('message',{
+		name: name,
 		text: $message.val()
 	});
 
