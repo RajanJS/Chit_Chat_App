076abd7bf2fc82ad0e66d8dfbbfcd209a0c7e25b
RajanMaharjan <razanmaharjan@gmail.com>
2016-02-26 23:42:21 +0545

    Add front-end code to submit msg

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,27 @@
-'use strict';	
+'use strict';
 
 var socket = io();
 
-socket.on('connect', function () {
+socket.on('connect', function() {
 	console.log('Connected to socket.io server!');
 });
 
-socket.on('message',function(message){
+socket.on('message', function(message) {
 	console.log('New message:');
 	console.log(message.text);
+});
+
+//handles submitting of new message
+var $form = jQuery('#message-form');
+
+$form.on('submit', function(event) {
+	event.preventDefault();
+
+	var $message = $form.find('input[name=message]');
+
+	socket.emit('message',{
+		text: $message.val()
+	});
+
+	$message.val('');
 });
